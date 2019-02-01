782d055630d790222f73fad49529c5b565a84a2d
R-WebsterNoble <R.WebsterNoble@gmail.com>
2019-02-01 22:57:04 +0000

    Added display types for all C# syntax

---
+++ VrCode/Assets/SyntaxNodes/Syntax.cs
@@ -0,0 +1,1006 @@
+﻿namespace Assets.SyntaxNodes
+{
+    public class CSharpSyntaxNode : Node
+    {
+    }
+
+    public class AliasedQualifiedNameSyntax : NameSyntax
+    {
+    }
+
+    public class AccessorDeclarationSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class AccessorListSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class AnonymousObjectMemberDeclaratorSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class ArgumentSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class ArrayRankSpecifierSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class ArrowExpressionClauseSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class AttributeArgumentListSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class AttributeArgumentSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class AttributeListSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class AttributeSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class AttributeTargetSpecifierSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class BaseArgumentListSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class ArgumentListSyntax : BaseArgumentListSyntax
+    {
+    }
+
+    public class BracketedArgumentListSyntax : BaseArgumentListSyntax
+    {
+    }
+
+    public class BaseCrefParameterListSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class CrefBracketedParameterListSyntax : BaseCrefParameterListSyntax
+    {
+    }
+
+    public class CrefParameterListSyntax : BaseCrefParameterListSyntax
+    {
+    }
+
+    public class BaseListSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class BaseParameterListSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class BracketedParameterListSyntax : BaseParameterListSyntax
+    {
+    }
+
+    public class ParameterListSyntax : BaseParameterListSyntax
+    {
+    }
+
+    public class BaseTypeSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class SimpleBaseTypeSyntax : BaseTypeSyntax
+    {
+    }
+
+    public class CatchClauseSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class CatchDeclarationSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class CatchFilterClauseSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class CompilationUnitSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class ConstructorInitializerSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class CrefParameterSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class CrefSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class MemberCrefSyntax : CrefSyntax
+    {
+    }
+
+    public class ConversionOperatorMemberCrefSyntax : MemberCrefSyntax
+    {
+    }
+
+    public class IndexerMemberCrefSyntax : MemberCrefSyntax
+    {
+    }
+
+    public class NameMemberCrefSyntax : MemberCrefSyntax
+    {
+    }
+
+    public class OperatorMemberCrefSyntax : MemberCrefSyntax
+    {
+    }
+
+    public class QualifiedCrefSyntax : CrefSyntax
+    {
+    }
+
+    public class TypeCrefSyntax : CrefSyntax
+    {
+    }
+
+    public class ElseClauseSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class EqualsValueClauseSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class ExplicitInterfaceSpecifierSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class ExpressionSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class AnonymousFunctionExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class AnonymousMethodExpressionSyntax : AnonymousFunctionExpressionSyntax
+    {
+    }
+
+    public class LambdaExpressionSyntax : AnonymousFunctionExpressionSyntax
+    {
+    }
+
+    public class ParenthesizedLambdaExpressionSyntax : LambdaExpressionSyntax
+    {
+    }
+
+    public class SimpleLambdaExpressionSyntax : LambdaExpressionSyntax
+    {
+    }
+
+    public class AnonymousObjectCreationExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class ArrayCreationExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class AssignmentExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class AwaitExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class BinaryExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class CastExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class CheckedExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class ConditionalAccessExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class ConditionalExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class DeclarationExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class DefaultExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class ElementAccessExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class ElementBindingExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class ImplicitArrayCreationExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class ImplicitElementAccessSyntax : ExpressionSyntax
+    {
+    }
+
+    public class ImplicitStackAllocArrayCreationExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class InitializerExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class InstanceExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class BaseExpressionSyntax : InstanceExpressionSyntax
+    {
+    }
+
+    public class ThisExpressionSyntax : InstanceExpressionSyntax
+    {
+    }
+
+    public class InterpolatedStringExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class InvocationExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class IsPatternExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class LiteralExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class MakeRefExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class MemberAccessExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class MemberBindingExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class ObjectCreationExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class OmittedArraySizeExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class ParenthesizedExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class PostfixUnaryExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class PrefixUnaryExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class QueryExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class RangeExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class RefExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class RefTypeExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class RefValueExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class SizeOfExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class StackAllocArrayCreationExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class SwitchExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class ThrowExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class TupleExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class TypeOfExpressionSyntax : ExpressionSyntax
+    {
+    }
+
+    public class TypeSyntax : ExpressionSyntax
+    {
+    }
+
+    public class ArrayTypeSyntax : TypeSyntax
+    {
+    }
+
+    public class NameSyntax : TypeSyntax
+    {
+    }
+
+    public class AliasQualifiedNameSyntax : NameSyntax
+    {
+    }
+
+    public class QualifiedNameSyntax : NameSyntax
+    {
+    }
+
+    public class SimpleNameSyntax : NameSyntax
+    {
+    }
+
+    public class GenericNameSyntax : SimpleNameSyntax
+    {
+    }
+
+    public class IdentifierNameSyntax : SimpleNameSyntax
+    {
+    }
+
+    public class NullableTypeSyntax : TypeSyntax
+    {
+    }
+
+    public class OmittedTypeArgumentSyntax : TypeSyntax
+    {
+    }
+
+    public class PointerTypeSyntax : TypeSyntax
+    {
+    }
+
+    public class PredefinedTypeSyntax : TypeSyntax
+    {
+    }
+
+    public class RefTypeSyntax : TypeSyntax
+    {
+    }
+
+    public class TupleTypeSyntax : TypeSyntax
+    {
+    }
+
+    public class ExternAliasDirectiveSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class FinallyClauseSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class InterpolatedStringContentSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class InterpolatedStringTextSyntax : InterpolatedStringContentSyntax
+    {
+    }
+
+    public class InterpolationSyntax : InterpolatedStringContentSyntax
+    {
+    }
+
+    public class InterpolationAlignmentClauseSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class InterpolationFormatClauseSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class JoinIntoClauseSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class MemberDeclarationSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class BaseFieldDeclarationSyntax : MemberDeclarationSyntax
+    {
+    }
+
+    public class EventFieldDeclarationSyntax : BaseFieldDeclarationSyntax
+    {
+    }
+
+    public class FieldDeclarationSyntax : BaseFieldDeclarationSyntax
+    {
+    }
+
+    public class BaseMethodDeclarationSyntax : MemberDeclarationSyntax
+    {
+    }
+
+    public class ConstructorDeclarationSyntax : BaseMethodDeclarationSyntax
+    {
+    }
+
+    public class ConversionOperatorDeclarationSyntax : BaseMethodDeclarationSyntax
+    {
+    }
+
+    public class DestructorDeclarationSyntax : BaseMethodDeclarationSyntax
+    {
+    }
+
+    public class MethodDeclarationSyntax : BaseMethodDeclarationSyntax
+    {
+    }
+
+    public class OperatorDeclarationSyntax : BaseMethodDeclarationSyntax
+    {
+    }
+
+    public class BasePropertyDeclarationSyntax : MemberDeclarationSyntax
+    {
+    }
+
+    public class EventDeclarationSyntax : BasePropertyDeclarationSyntax
+    {
+    }
+
+    public class IndexerDeclarationSyntax : BasePropertyDeclarationSyntax
+    {
+    }
+
+    public class PropertyDeclarationSyntax : BasePropertyDeclarationSyntax
+    {
+    }
+
+    public class BaseTypeDeclarationSyntax : MemberDeclarationSyntax
+    {
+    }
+
+    public class EnumDeclarationSyntax : BaseTypeDeclarationSyntax
+    {
+    }
+
+    public class TypeDeclarationSyntax : BaseTypeDeclarationSyntax
+    {
+    }
+
+    public class ClassDeclarationSyntax : TypeDeclarationSyntax
+    {
+    }
+
+    public class InterfaceDeclarationSyntax : TypeDeclarationSyntax
+    {
+    }
+
+    public class StructDeclarationSyntax : TypeDeclarationSyntax
+    {
+    }
+
+    public class DelegateDeclarationSyntax : MemberDeclarationSyntax
+    {
+    }
+
+    public class EnumMemberDeclarationSyntax : MemberDeclarationSyntax
+    {
+    }
+
+    public class GlobalStatementSyntax : MemberDeclarationSyntax
+    {
+    }
+
+    public class IncompleteMemberSyntax : MemberDeclarationSyntax
+    {
+    }
+
+    public class NamespaceDeclarationSyntax : MemberDeclarationSyntax
+    {
+    }
+
+    public class NameColonSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class NameEqualsSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class OrderingSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class ParameterSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class PatternSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class ConstantPatternSyntax : PatternSyntax
+    {
+    }
+
+    public class DeclarationPatternSyntax : PatternSyntax
+    {
+    }
+
+    public class DiscardPatternSyntax : PatternSyntax
+    {
+    }
+
+    public class RecursivePatternSyntax : PatternSyntax
+    {
+    }
+
+    public class VarPatternSyntax : PatternSyntax
+    {
+    }
+
+    public class PositionalPatternClauseSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class PropertyPatternClauseSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class QueryBodySyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class QueryClauseSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class FromClauseSyntax : QueryClauseSyntax
+    {
+    }
+
+    public class JoinClauseSyntax : QueryClauseSyntax
+    {
+    }
+
+    public class LetClauseSyntax : QueryClauseSyntax
+    {
+    }
+
+    public class OrderByClauseSyntax : QueryClauseSyntax
+    {
+    }
+
+    public class WhereClauseSyntax : QueryClauseSyntax
+    {
+    }
+
+    public class QueryContinuationSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class SelectOrGroupClauseSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class GroupClauseSyntax : SelectOrGroupClauseSyntax
+    {
+    }
+
+    public class SelectClauseSyntax : SelectOrGroupClauseSyntax
+    {
+    }
+
+    public class StatementSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class BlockSyntax : StatementSyntax
+    {
+    }
+
+    public class BreakStatementSyntax : StatementSyntax
+    {
+    }
+
+    public class CheckedStatementSyntax : StatementSyntax
+    {
+    }
+
+    public class CommonForEachStatementSyntax : StatementSyntax
+    {
+    }
+
+    public class ForEachStatementSyntax : CommonForEachStatementSyntax
+    {
+    }
+
+    public class ForEachVariableStatementSyntax : CommonForEachStatementSyntax
+    {
+    }
+
+    public class ContinueStatementSyntax : StatementSyntax
+    {
+    }
+
+    public class DoStatementSyntax : StatementSyntax
+    {
+    }
+
+    public class EmptyStatementSyntax : StatementSyntax
+    {
+    }
+
+    public class ExpressionStatementSyntax : StatementSyntax
+    {
+    }
+
+    public class FixedStatementSyntax : StatementSyntax
+    {
+    }
+
+    public class ForStatementSyntax : StatementSyntax
+    {
+    }
+
+    public class GotoStatementSyntax : StatementSyntax
+    {
+    }
+
+    public class IfStatementSyntax : StatementSyntax
+    {
+    }
+
+    public class LabeledStatementSyntax : StatementSyntax
+    {
+    }
+
+    public class LocalDeclarationStatementSyntax : StatementSyntax
+    {
+    }
+
+    public class LocalFunctionStatementSyntax : StatementSyntax
+    {
+    }
+
+    public class LockStatementSyntax : StatementSyntax
+    {
+    }
+
+    public class ReturnStatementSyntax : StatementSyntax
+    {
+    }
+
+    public class SwitchStatementSyntax : StatementSyntax
+    {
+    }
+
+    public class ThrowStatementSyntax : StatementSyntax
+    {
+    }
+
+    public class TryStatementSyntax : StatementSyntax
+    {
+    }
+
+    public class UnsafeStatementSyntax : StatementSyntax
+    {
+    }
+
+    public class UsingStatementSyntax : StatementSyntax
+    {
+    }
+
+    public class WhileStatementSyntax : StatementSyntax
+    {
+    }
+
+    public class YieldStatementSyntax : StatementSyntax
+    {
+    }
+
+    public class StructuredTriviaSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class DirectiveTriviaSyntax : StructuredTriviaSyntax
+    {
+    }
+
+    public class BadDirectiveTriviaSyntax : DirectiveTriviaSyntax
+    {
+    }
+
+    public class BranchingDirectiveTriviaSyntax : DirectiveTriviaSyntax
+    {
+    }
+
+    public class ConditionalDirectiveTriviaSyntax : BranchingDirectiveTriviaSyntax
+    {
+    }
+
+    public class ElifDirectiveTriviaSyntax : ConditionalDirectiveTriviaSyntax
+    {
+    }
+
+    public class IfDirectiveTriviaSyntax : ConditionalDirectiveTriviaSyntax
+    {
+    }
+
+    public class ElseDirectiveTriviaSyntax : BranchingDirectiveTriviaSyntax
+    {
+    }
+
+    public class DefineDirectiveTriviaSyntax : DirectiveTriviaSyntax
+    {
+    }
+
+    public class EndIfDirectiveTriviaSyntax : DirectiveTriviaSyntax
+    {
+    }
+
+    public class EndRegionDirectiveTriviaSyntax : DirectiveTriviaSyntax
+    {
+    }
+
+    public class ErrorDirectiveTriviaSyntax : DirectiveTriviaSyntax
+    {
+    }
+
+    public class LineDirectiveTriviaSyntax : DirectiveTriviaSyntax
+    {
+    }
+
+    public class LoadDirectiveTriviaSyntax : DirectiveTriviaSyntax
+    {
+    }
+
+    public class NullableDirectiveTriviaSyntax : DirectiveTriviaSyntax
+    {
+    }
+
+    public class PragmaChecksumDirectiveTriviaSyntax : DirectiveTriviaSyntax
+    {
+    }
+
+    public class PragmaWarningDirectiveTriviaSyntax : DirectiveTriviaSyntax
+    {
+    }
+
+    public class ReferenceDirectiveTriviaSyntax : DirectiveTriviaSyntax
+    {
+    }
+
+    public class RegionDirectiveTriviaSyntax : DirectiveTriviaSyntax
+    {
+    }
+
+    public class ShebangDirectiveTriviaSyntax : DirectiveTriviaSyntax
+    {
+    }
+
+    public class UndefDirectiveTriviaSyntax : DirectiveTriviaSyntax
+    {
+    }
+
+    public class WarningDirectiveTriviaSyntax : DirectiveTriviaSyntax
+    {
+    }
+
+    public class DocumentationCommentTriviaSyntax : StructuredTriviaSyntax
+    {
+    }
+
+    public class SkippedTokensTriviaSyntax : StructuredTriviaSyntax
+    {
+    }
+
+    public class SubpatternSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class SwitchExpressionArmSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class SwitchLabelSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class CasePatternSwitchLabelSyntax : SwitchLabelSyntax
+    {
+    }
+
+    public class CaseSwitchLabelSyntax : SwitchLabelSyntax
+    {
+    }
+
+    public class DefaultSwitchLabelSyntax : SwitchLabelSyntax
+    {
+    }
+
+    public class SwitchSectionSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class TupleElementSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class TypeArgumentListSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class TypeParameterConstraintClauseSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class TypeParameterConstraintSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class ClassOrStructConstraintSyntax : TypeParameterConstraintSyntax
+    {
+    }
+
+    public class ConstructorConstraintSyntax : TypeParameterConstraintSyntax
+    {
+    }
+
+    public class TypeConstraintSyntax : TypeParameterConstraintSyntax
+    {
+    }
+
+    public class TypeParameterListSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class TypeParameterSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class UsingDirectiveSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class VariableDeclarationSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class VariableDeclaratorSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class VariableDesignationSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class DiscardDesignationSyntax : VariableDesignationSyntax
+    {
+    }
+
+    public class ParenthesizedVariableDesignationSyntax : VariableDesignationSyntax
+    {
+    }
+
+    public class SingleVariableDesignationSyntax : VariableDesignationSyntax
+    {
+    }
+
+    public class WhenClauseSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class XmlAttributeSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class XmlCrefAttributeSyntax : XmlAttributeSyntax
+    {
+    }
+
+    public class XmlNameAttributeSyntax : XmlAttributeSyntax
+    {
+    }
+
+    public class XmlTextAttributeSyntax : XmlAttributeSyntax
+    {
+    }
+
+    public class XmlElementEndTagSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class XmlElementStartTagSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class XmlNameSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class XmlNodeSyntax : CSharpSyntaxNode
+    {
+    }
+
+    public class XmlCDataSectionSyntax : XmlNodeSyntax
+    {
+    }
+
+    public class XmlCommentSyntax : XmlNodeSyntax
+    {
+    }
+
+    public class XmlElementSyntax : XmlNodeSyntax
+    {
+    }
+
+    public class XmlEmptyElementSyntax : XmlNodeSyntax
+    {
+    }
+
+    public class XmlProcessingInstructionSyntax : XmlNodeSyntax
+    {
+    }
+
+    public class XmlTextSyntax : XmlNodeSyntax
+    {
+    }
+
+    public class XmlPrefixSyntax : CSharpSyntaxNode
+    {
+    }
+}
